2311660882419180ec6a0a1cf5d7d7b180cf0f6d
Thomas Bury <tombury182@gmail.com>
2018-11-01 19:03:52 -0400

    initial upload of ews functions in python

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from test_fn import add_numbers
+
+a=3;
+b=2;
+z=add_numbers(a,b)
+print("the sum of",a,"and",b,"is",z)
+
---
+++ test_fn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 15:31:02 2018
+
+@author: tb460
+"""
+
+
+def my_function():
+  print("Hello from a function")
+
+def add_numbers(a,b):
+    return a+b
+
+def multi_numbers(a,b):
+    return a*b
