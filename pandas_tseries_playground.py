5c5a82ddca5f383012cf8834798a3169891cf88f
Thomas Bury <tombury182@gmail.com>
2018-11-03 17:09:09 -0400

    update to rolling window metrics

--- pandas_tseries_playground.py
+++ pandas_tseries_playground.py
@@ -11,29 +11,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # create a noisy trajectory
-t=np.linspace(1,10,100)
+t=np.linspace(0,10,110)
 x=np.random.randn(len(t))*0.5
 
-data={'happy rating':x}
-df=pd.DataFrame(data)
+# create a dataframe indexed by time
+data={'state variable':x}
+df=pd.DataFrame(data,index=t)
+df.index.rename('Time',inplace=True)
 
-roll_window=4
+# parameters
+roll_window=20
+lag_time=1
 
 # compute rolling stats
 roll_var=df.rolling(window=roll_window).var()
-#roll_ac=df.rolling(window=roll_window).apply(lambda x: x.autocorr(lag=1),raw=False)
+roll_ac=pd.rolling_apply(
+        df.iloc[:,0],
+        window=roll_window,
+        func=lambda x: pd.Series(x).autocorr(lag=lag_time))
 roll_std=df.rolling(window=roll_window).std()
 roll_skew=df.rolling(window=roll_window).skew()
 
 
-       
-       
 # add rolling stats to dataframe
 df.loc[:,'rolling var']=roll_var.iloc[:,0]
-#df.loc[:,'rolling ac']=roll_ac.iloc[:,0]
+df.loc[:,'rolling ac']=roll_ac
 df.loc[:,'rolling std']=roll_std.iloc[:,0]
 df.loc[:,'rolling skew']=roll_skew.iloc[:,0]
-
 
 
 plt.figure();
