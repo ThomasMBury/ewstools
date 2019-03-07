d80c9fb479a021a7c0b6ccfc2c1c9aef57f6ce75
Thomas Bury <tombury182@gmail.com>
2019-03-07 11:51:48 -0500

    update setup info including dependencies

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = ["lmfit>=0.9", "arch>=4.7"]
+
 setuptools.setup(
     name="ewstools",
     version="0.0.1",
     author="Thomas M Bury",
-    author_email="tbury@uwaterloo.ca",
-    description="Compute early warning signals from time-series data",
+    author_email="tombury182@gmail.com",
+    description="""A package with tools to compute early warning signals (EWS) 
+    from time-series data""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ThomasMBury/ewstools",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
