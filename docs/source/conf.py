3214da9cdd15955e54b8da5ff6957ebd39f8d8bc
Thomas Bury <tombury182@gmail.com>
2019-03-18 21:49:25 +0000

    napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ release = '0.0.2'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
