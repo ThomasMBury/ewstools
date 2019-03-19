daa4b01a67991ef6b2476234918b9de0450e7161
Thomas Bury <tombury182@gmail.com>
2019-03-19 17:53:39 +0000

    add bootstrap functions to ewstools

--- ewstools/ewstools.py
+++ ewstools/ewstools.py
@@ -39,18 +39,19 @@
 import numpy as np
 import pandas as pd
 
-# To compute power spectrum using Welch's method
-from scipy import signal
+
+# Module for block-bootstrapping time-series
+from arch.bootstrap import StationaryBootstrap, CircularBlockBootstrap
+
 
 # For detrending time-series
 from statsmodels.nonparametric.smoothers_lowess import lowess
 from scipy.ndimage.filters import gaussian_filter as gf
 
-# For fitting power spectrum models and computing AIC weights
-from lmfit import Model
+
 
 # Import helper functions
-from ewstools.helperfuns import *
+from ewstools import helperfuns
 
 
 
@@ -260,7 +261,7 @@ def ews_compute(raw_series,
             t_point = eval_series.index[k+(rw_size-1)]            
             
             ## Compute the power spectrum using function pspec_welch
-            pspec = pspec_welch(window_series, dt, 
+            pspec = helperfuns.pspec_welch(window_series, dt, 
                                 ham_length=ham_length, 
                                 ham_offset=ham_offset,
                                 w_cutoff=w_cutoff,
@@ -268,7 +269,7 @@ def ews_compute(raw_series,
             
             
             ## Compute the spectral EWS using pspec_metrics (dictionary)
-            metrics = pspec_metrics(pspec, ews, sweep)
+            metrics = helperfuns.pspec_metrics(pspec, ews, sweep)
             # Add the time-stamp
             metrics['Time'] = t_point
             # Add metrics (dictionary) to the list
@@ -281,14 +282,14 @@ def ews_compute(raw_series,
                 # Create fine-scale frequency values
                 wVals = np.linspace(min(pspec.index), max(pspec.index), 100)
                 # Fold fit
-                pspec_fold = psd_fold(wVals, metrics['Params fold']['sigma'],
+                pspec_fold = helperfuns.psd_fold(wVals, metrics['Params fold']['sigma'],
                      metrics['Params fold']['lam'])
                 # Hopf fit
-                pspec_hopf = psd_hopf(wVals, metrics['Params hopf']['sigma'],
+                pspec_hopf = helperfuns.psd_hopf(wVals, metrics['Params hopf']['sigma'],
                      metrics['Params hopf']['mu'],
                      metrics['Params hopf']['w0'])
                 # Null fit
-                pspec_null = psd_null(wVals, metrics['Params null']['sigma'])
+                pspec_null = helperfuns.psd_null(wVals, metrics['Params null']['sigma'])
                 
                 ## Put spectrum fits into a dataframe
                 dic_temp = {'Time': t_point*np.ones(len(wVals)), 
@@ -369,6 +370,241 @@ def ews_compute(raw_series,
 
 
    
+    
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Bootstrapping
+#–--------------------------------------------
+
+def block_bootstrap(series,
+              n_samples,
+              bs_type = 'Stationary',
+              block_size = 10
+              ):
+
+    '''
+    Computes block-bootstrap samples of series.
+    
+    Args
+    ----
+    series: pd.Series
+        Time-series data in the form of a Pandas Series indexed by time
+    n_samples: int
+        Number of bootstrapped samples to output.
+    bs_type: {'Stationary', 'Circular'}
+        Type of block-bootstrapping to perform.
+    block_size: int
+        Size of resampling blocks. Should be big enough to
+        capture important frequencies in the series.
+        
+    Returns
+    -------
+    pd.DataFrame:
+        DataFrame containing the block-bootstrapped samples of series. 
+        Indexed by sample number, then time.
+    
+    '''
+
+    # Set up list for sampled time-series
+    list_samples = []
+    
+    # Stationary bootstrapping
+    if bs_type == 'Stationary':
+        bs = StationaryBootstrap(block_size, series)
+                
+        # Count for sample number
+        count = 1
+        for data in bs.bootstrap(n_samples):
+            
+            df_temp = pd.DataFrame({'sample': count, 
+                                    'time': series.index.values,
+                                    'x': data[0][0]})
+            list_samples.append(df_temp)
+            count += 1
+            
+    if bs_type == 'Circular':
+        bs = CircularBlockBootstrap(block_size, series)
+                
+        # Count for sample number
+        count = 1
+        for data in bs.bootstrap(n_samples):
+            
+            df_temp = pd.DataFrame({'sample': count, 
+                                    'time': series.index.values,
+                                    'x': data[0][0]})
+            list_samples.append(df_temp)
+            count += 1   
+    
+
+    # Concatenate list of samples
+    df_samples = pd.concat(list_samples)
+    df_samples.set_index(['sample','time'], inplace=True)
+
+    
+    # Output DataFrame of samples
+    return df_samples
+
+    
+
+def roll_bootstrap(raw_series,
+                   span = 0.1,
+                   roll_window = 0.25,
+                   roll_offset = 1,
+                   upto = 'Full',
+                   n_samples = 20,
+                   bs_type = 'Stationary',
+                   block_size = 10
+                   ):
+    
+    
+    '''
+    Smooths raw_series and computes residuals over a rolling window.
+    Bootstraps each segment and outputs samples.
+    
+    Args
+    ----
+    raw_series: pd.Series 
+        Time-series data in the form of a Pandas Seires indexed by time.
+    span: float
+        Proportion of data used for Loess filtering.
+    roll_windopw: float
+        Size of the rolling window (as a proportion
+        of the length of the data).
+    roll_offset: int
+        Number of data points to shift the rolling window
+        upon each iteration (reduce to increase computation time).
+    upto: int/'Full'
+        If 'Full', use entire time-series, otherwise input time up 
+        to which EWS are to be evaluated.
+    n_samples: int
+        Number of bootstrapped samples to output.
+    bs_type: {'Stationary', 'Circular'}
+        Type of block-bootstrapping to perform.
+    block_size: int
+        Size of resampling blocks. Should be big enough to
+        capture important frequencies in the series.
+        
+        
+    Return
+    ------
+    pd.DataFrame:
+        DataFrame containing the block-bootstrapped samples at each time
+        in raw_series. Indexed by time in raw_series, then, sample number,
+        then time within the rolling window.
+        
+    '''
+
+    
+    
+    ## Parameter configuration
+    
+    # Compute the rolling window size (integer value)
+    rw_size=int(np.floor(roll_window * raw_series.shape[0]))
+    
+    
+    # Compute the Lowess span as a proportion if given as absolute
+    if not 0 < span <= 1:
+        span = span/raw_series.shape[0]
+    else:
+        span = span
+    
+
+    ## Data detrending
+
+    # Select portion of data up to 'upto'
+    if upto == 'Full':
+        series = raw_series
+    else: series = raw_series.loc[:upto]
+    
+    
+
+    
+    # Smooth the series and compute the residuals
+    smooth_data = lowess(series.values, series.index.values, frac=span)[:,1]
+    residuals = series.values - smooth_data
+    resid_series = pd.Series(residuals, index=series.index)
+
+
+
+
+    ## Rolling window over residuals
+
+    
+    # Number of components in the residual time-series
+    num_comps = len(resid_series)
+    # Make sure window offset is an integer
+    roll_offset = int(roll_offset)
+    
+    # Initialise a list for the sample residuals at each time point
+    list_samples = []
+    
+    # Counter
+    i=0
+    
+    # Loop through window locations shifted by roll_offset
+    for k in np.arange(0, num_comps-(rw_size-1), roll_offset):
+        
+        # Select subset of series contained in window
+        window_series = resid_series.iloc[k:k+rw_size]           
+        # Asisgn the time value for the metrics (right end point of window)
+        t_point = resid_series.index[k+(rw_size-1)]            
+        
+        # Compute bootstrap samples of residauls within rolling window
+        df_samples_temp = block_bootstrap(window_series, n_samples, 
+                                          bs_type, block_size)
+        
+        # Add column with real time (end of window)
+        df_samples_temp['Time'] = t_point
+                
+        # Reorganise index
+        df_samples_temp.reset_index(inplace=True)
+        df_samples_temp.set_index(['Time','sample','time'], inplace=True)
+        df_samples_temp.index.rename(['Time','Sample','Wintime'],inplace=True)
+        
+        # Append the list of samples
+        list_samples.append(df_samples_temp)
+        
+#        # Print update
+#        if i % 1 ==0:
+#            print('Bootstrap samples for window at t = %.2f complete' % (t_point))
+            
+        i += 1
+    
+
+
+
+    ## Organise output DataFrame
+
+    
+    # Concatenate list of samples
+    df_samples = pd.concat(list_samples)
+    
+    # Output DataFrame
+    return df_samples
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
    
 
     
