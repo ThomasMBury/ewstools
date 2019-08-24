8ae585b9268a91bcea1eae56a048797a62a7271b
Thomas Bury <tombury182@gmail.com>
2019-08-24 17:38:16 -0400

    fix bugs

--- tests/test_ewstools.py
+++ tests/test_ewstools.py
@@ -10,7 +10,8 @@ import pandas as pd
 
 
 # Import ewstools
-import ewstools
+from ewstools import core
+from ewstools import helpers
 
 def test_ews_compute():
     '''
@@ -25,7 +26,7 @@ def test_ews_compute():
     # Run through ews_compute with all possible EWS
     ews = ['var','ac','sd','cv','skew','kurt','smax','aic','cf','smax/var','smax/mean']
     lag_times = [1,2,3,4,5]
-    dict_ews = ewstools.core.ews_compute(series,
+    dict_ews = core.ews_compute(series,
                              ews=ews,
                              lag_times=lag_times,
                              sweep = True
@@ -54,7 +55,7 @@ def test_pspec_welch():
     dt = 1
     ham_length = 40
     yVals = np.random.normal(0,1,n_points)
-    pspec = ewstools.helpers.pspec_welch(yVals, dt, ham_length=ham_length)
+    pspec = helpers.pspec_welch(yVals, dt, ham_length=ham_length)
     
     assert type(pspec) == pd.Series
     assert pspec.shape in [(n_points,),
@@ -70,9 +71,9 @@ def test_psd_forms():
     lamda = -0.1
     mu = -0.1
     w0 = 1
-    sFoldVals = ewstools.helpers.psd_fold(wVals, sigma, lamda)
-    sHopfVals = ewstools.helpers.psd_hopf(wVals, sigma, mu, w0)
-    sNullVals = ewstools.helpers.psd_null(wVals, sigma)
+    sFoldVals = helpers.psd_fold(wVals, sigma, lamda)
+    sHopfVals = helpers.psd_hopf(wVals, sigma, mu, w0)
+    sNullVals = helpers.psd_null(wVals, sigma)
         
     assert type(sFoldVals)==np.ndarray
     assert type(sHopfVals)==np.ndarray
@@ -88,7 +89,7 @@ def test_sfold_init():
     stot = 1
     smax = 0.5
     
-    [sigma, lamda] = ewstools.helpers.sfold_init(smax, stot)
+    [sigma, lamda] = helpers.sfold_init(smax, stot)
     
     # Values that smax, stot should attain (+/- 1dp)
     smax_assert = sigma**2/(2*np.pi*lamda**2)
@@ -107,7 +108,7 @@ def test_shopf_init():
     stot = 1
     wdom = 1
     
-    [sigma, mu, w0] = ewstools.helpers.shopf_init(smax, stot, wdom)
+    [sigma, mu, w0] = helpers.shopf_init(smax, stot, wdom)
     
     # Values that smax, stot should attain (+/- 1dp)
     smax_assert = (sigma**2/(4*np.pi*mu**2))*(1+(mu**2/(mu**2+4*w0**2)))
@@ -127,7 +128,7 @@ def test_snull_init():
     '''
     stot = 1
     
-    [sigma] = ewstools.helpers.snull_init(stot)
+    [sigma] = helpers.snull_init(stot)
     
     # Values that smax, stot should attain (+/- 1dp)
     stot_assert = sigma**2
@@ -147,13 +148,13 @@ def test_fit_fold():
     dt = 1
     ham_length = 40
     yVals = np.random.normal(0,1,n_points)
-    pspec = ewstools.helpers.pspec_welch(yVals, dt, ham_length=ham_length)
+    pspec = helpers.pspec_welch(yVals, dt, ham_length=ham_length)
     
     sigma_init = 0.05
     lambda_init = -0.1
     init = [sigma_init, lambda_init]
     # Run power spectrum in fit_fold
-    [aic, model] = ewstools.helpers.fit_fold(pspec, init)
+    [aic, model] = helpers.fit_fold(pspec, init)
     
     assert type(aic) == np.float64
     assert type(model.values) == dict
@@ -170,14 +171,14 @@ def test_fit_hopf():
     dt = 1
     ham_length = 40
     yVals = np.random.normal(0,1,n_points)
-    pspec = ewstools.helpers.pspec_welch(yVals, dt, ham_length=ham_length)
+    pspec = helpers.pspec_welch(yVals, dt, ham_length=ham_length)
     
     sigma_init = 0.05
     mu_init = -0.1
     w0_init = 1
     init = [sigma_init, mu_init, w0_init]
     # Run power spectrum in fit_hopf
-    [aic, model] = ewstools.helpers.fit_hopf(pspec, init)
+    [aic, model] = helpers.fit_hopf(pspec, init)
     
     assert type(aic) == np.float64
     assert type(model.values) == dict
@@ -194,12 +195,12 @@ def test_fit_null():
     dt = 1
     ham_length = 40
     yVals = np.random.normal(0,1,n_points)
-    pspec = ewstools.helpers.pspec_welch(yVals, dt, ham_length=ham_length)
+    pspec = helpers.pspec_welch(yVals, dt, ham_length=ham_length)
     
     sigma_init = 0.05
     init = [sigma_init]
     # Run power spectrum in fit_null
-    [aic, model] = ewstools.helperfuns.fit_null(pspec, init)
+    [aic, model] = helpers.fit_null(pspec, init)
     
     assert type(aic) == np.float64
     assert type(model.values) == dict
@@ -211,7 +212,7 @@ def test_aic_weights():
     '''
     aic_scores = np.array([-231,-500,-100,5])
     
-    aic_weights = ewstools.helpers.aic_weights(aic_scores)
+    aic_weights = helpers.aic_weights(aic_scores)
     
     assert type(aic_weights) == np.ndarray
     
@@ -226,10 +227,10 @@ def test_pspec_metrics():
     dt = 1
     ham_length = 40
     yVals = np.random.normal(0,1,n_points)
-    pspec = ewstools.helpers.pspec_welch(yVals, dt, ham_length=ham_length)
+    pspec = helpers.pspec_welch(yVals, dt, ham_length=ham_length)
     
     # Run power spectrum in pspec_metrics
-    spec_ews = ewstools.helperfuns.pspec_metrics(pspec,
+    spec_ews = helpers.pspec_metrics(pspec,
                       ews=['smax','cf','aic'],
                       sweep=True)
 
@@ -253,7 +254,7 @@ def test_block_bootstrap():
     bs_types = ['Stationary', 'Circular']
     # Run through block_bootstrap
     for bs_type in bs_types:
-        samples = ewstools.core.block_bootstrap(series,
+        samples = core.block_bootstrap(series,
                                        n_samples,
                                        bs_type=bs_type,
                                        block_size=block_size
@@ -278,7 +279,7 @@ def test_roll_bootstrap():
     n_samples = 2
     
     # Run function
-    df_bootstrap = ewstools.core.roll_bootstrap(series,
+    df_bootstrap = core.roll_bootstrap(series,
                                            n_samples=n_samples)
 
     assert type(df_bootstrap) == pd.DataFrame
@@ -295,10 +296,10 @@ def test_mean_ci():
     data = pd.Series(np.random.normal(loc=0,scale=1,size=100))
     
     # Compute confidence intevals
-    intervals = ewstools.core.mean_ci(data)
+    intervals = core.mean_ci(data)
     
     assert type(intervals) == dict
-    assert type(intervals['Mean']) == np.float64
+    assert type(intervals['Mean']) == float
 
 
     
