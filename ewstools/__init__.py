617ae4bfa6a83a79e284d8ad7c33b79989387930
Thomas Bury <tombury182@gmail.com>
2019-08-24 15:02:39 -0400

    move functions to ewstools.py

--- ewstools/__init__.py
+++ ewstools/__init__.py
@@ -1,1 +1,3 @@
+# __init__.py
 name = "ewstools"
+
--- ewstools/helperfuns.py
+++ ewstools/helperfuns.py
@@ -43,7 +43,6 @@ import pandas as pd
 from scipy import signal
 
 
-
 # For fitting power spectrum models and computing AIC weights
 from lmfit import Model
 
@@ -66,7 +65,7 @@ def pspec_welch(yVals,
     time-series and then takes the average.
     
     Args
-    ------------------
+    ----
     yVals: array of floats
         Array of time-series values.
     dt: float
@@ -85,7 +84,7 @@ def pspec_welch(yVals,
         is the power spectrum normalised (such that the area underneath equals one).        
             
     Returns
-    --------------------
+    -------
     pd.Series: 
         Power values indexed by frequency
         
@@ -177,7 +176,7 @@ def shopf_init(smax, stot, wdom):
     when fitting sHopf to the empirical power spectrum.
     
     Args
-    --------------
+    ----
     smax: float
         Maximum power in the power spectrum.
     stot: float
@@ -186,7 +185,7 @@ def shopf_init(smax, stot, wdom):
         Frequency that has the highest power.
         
     Return
-    -----------------
+    ------
     list of floats: 
         List containing the initialisation parameters [sigma, mu, w0]
         
@@ -673,6 +672,131 @@ def pspec_metrics(pspec,
     return spec_ews
 
     
+
+
+
+
+
+#------------------------
+##  Function to compute lag-1 autocovariance matrix
+
+def compute_autocov(df_in):
+    '''
+    Computes the autocovariance (lag-1) matrix of n 
+    time series provided in df_in.
+    
+    Using the definition
+        phi_ij = < X_i(t+1) X_j(t) >
+    for each element of the autocovariance matrix phi.
+    
+    Input:
+        df_in: DataFrame with n columns indexed by time
+    Ouptut:
+        np.array of autocovariance matrix
+    '''
+    
+    # Obtain column names of df_in
+    col_names = df_in.columns
+    # Number of variables
+    n = len(col_names)
+    
+    
+    # Define function to compute autocovariance of two columns
+    def autocov_cols(a,b):
+        '''
+        Computes autocovariance of two columns (can be the same)
+        Note that this does not commute (a<->b) in general
+        Input:
+            a,b: Series indexed by time
+        Output:
+            float: autocovariance between the columns
+        '''
+        
+        # Shift the column of a by 1
+        a_shift = a.shift(1)
+        
+        # Put into a dataframe
+        df_temp = pd.concat([a_shift,b], axis=1)
+        
+        # Compute covariance of columns a and b_shift
+        cov = df_temp.cov().iloc[0,1]
+        
+        # Output
+        return cov
+            
+        
+    # Compute elements of autocovariance matrix
+    list_elements = []
+    
+    for i in range(n):
+        for j in range(n):
+            a = df_in[col_names[i]]
+            b = df_in[col_names[j]]
+            # Compute autocovaraince between cols
+            autocov = autocov_cols(a,b)
+            # Append to list of elements
+            list_elements.append(autocov)
+    
+    # Create autocovariance matrix from list of elements
+    ar_autocov = np.array(list_elements).reshape(n,n)
+
+    # Output
+    return ar_autocov
+
+
+
+
+
+
+
+
+
+#---------------------------------------
+## Function to do Jacobian and eval reconstruction 
+
+
+def eval_recon(df_in):
+    '''
+    Constructs estimate of Jacobian matrix from stationary time-series data
+    and outputs the eigenvalues
+    Input:
+        df_in: DataFrame with two columns indexed by time
+    Output:
+    	dictionary consisting of
+    		- 'Eigenvalues': np.array of eigenvalues
+    		- 'Eigenvectors': np.array of eigenvectors
+    		- 'Jacobian': pd.DataFrame of Jacobian entries
+    '''
+    
+    # Compute autocovaraince matrix from columns
+    ar_autocov = compute_autocov(df_in)
+    
+    # Compute the covariance matrix (built in function)
+    ar_cov = df_in.cov()
+    
+    # Estimate of Jacobian (formula in Williamson (2015))
+    # Requires computation of an inverse matrix
+    jac = np.matmul(ar_autocov, np.linalg.inv(ar_cov))
+    # Write the Jacobian as a df for output (so we have col lables)
+    df_jac = pd.DataFrame(jac, columns = df_in.columns, index=df_in.columns)
+      
+    # Compute eigenvalues and eigenvectors
+    evals, evecs = np.linalg.eig(jac)
+
+    # Dictionary of data output
+    dic_out = {'Eigenvalues':evals, 
+               'Eigenvectors':evecs,
+               'Jacobian':df_jac}
+    
+    return dic_out
+
+
+
+
+
+
+
+
    
 
 
