3b742ed40486931c2d06c95444de98cd44ae230b
Thomas Bury <tombury182@gmail.com>
2022-07-11 13:03:41 -0400

    update path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../ewstools/'))
 
 
 # mock import these packages because readthedocs doesn't have them installed
@@ -91,7 +91,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
